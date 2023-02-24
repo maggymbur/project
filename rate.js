658099b328647d9a61bf3fdaa5ea388dd4e19e59
maggymbur <mburumaggy62@gmail.com>
2023-02-24 09:17:19 +0300

    update files

---
+++ rate.js
@@ -0,0 +1,19 @@
+ // Function to handle the star rating system
+ function handleRating(rating) {
+    // Reset all stars to empty
+    document.querySelectorAll('.star').forEach(function(star) {
+        star.classList.add('empty');
+    });
+
+    // Fill in the selected number of stars
+    for (var i = 0; i < rating; i++) {
+        document.querySelectorAll('.star')[i].classList.remove('empty');
+    }
+}
+
+// Add event listeners to the stars to handle clicks
+document.querySelectorAll('.star').forEach(function(star, index) {
+    star.addEventListener('click', function() {
+        handleRating(index + 1);
+    });
+});
