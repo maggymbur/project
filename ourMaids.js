5e38f41fe4e342e63d20bce81f57a89c58deb480
maggymbur <mburumaggy62@gmail.com>
2023-02-25 15:28:33 +0300

    modified files

---
+++ ourMaids.js
@@ -0,0 +1,13 @@
+// Get all the submit buttons on the page
+const buttons = document.querySelectorAll('button[type="submit"]');
+
+// Add an event listener to each button
+buttons.forEach(button => {
+  button.addEventListener('click', function(event) {
+    // Prevent the form from submitting and reloading the page
+    event.preventDefault();
+
+    // Display an alert message
+    alert('Thank yoy for choosing. Check your email for more information');
+  });
+});
--- rate.js
+++ rate.js
@@ -1,19 +1,22 @@
  // Function to handle the star rating system
- function handleRating(rating) {
-    // Reset all stars to empty
-    document.querySelectorAll('.star').forEach(function(star) {
-        star.classList.add('empty');
-    });
+function handleRating(rating) {
+	// Reset all stars to empty
+	document.querySelectorAll('.star').forEach(function(star) {
+		star.classList.add('empty');
+	});
 
-    // Fill in the selected number of stars
-    for (var i = 0; i < rating; i++) {
-        document.querySelectorAll('.star')[i].classList.remove('empty');
-    }
+	// Fill in the selected number of stars
+	for (var i = 0; i < rating; i++) {
+		document.querySelectorAll('.star')[i].classList.remove('empty');
+	}
+
+	// Display alert message
+	alert("Thanks for rating this page!");
 }
 
 // Add event listeners to the stars to handle clicks
 document.querySelectorAll('.star').forEach(function(star, index) {
-    star.addEventListener('click', function() {
-        handleRating(index + 1);
-    });
+	star.addEventListener('click', function() {
+		handleRating(index + 1);
+	});
 });
